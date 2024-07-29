Implemented correction when calculating a weak classifier. Fixed scale calculation cycle

IntegralImage.py
@@ -17,11 +17,24 @@ def to_integral_image(img_arr):
     return integral_image_arr
 
 
-def sum_region(integral_img_arr, top_left, bottom_right):
-    # swap tuples
-    top_left = (top_left[1], top_left[0])
-    bottom_right = (bottom_right[1], bottom_right[0])
+def to_integral_image_of_squares(img_arr):
+    integral_image_of_squares = np.zeros((img_arr.shape[0], img_arr.shape[1]))
+    for x in range(img_arr.shape[1]):
+        row_sum = 0
 
+        for y in range(img_arr.shape[0]):
+            value_square = pow(img_arr[y, x], 2)
+
+            integral_image_of_squares[y, x] = \
+                (integral_image_of_squares[y, x - 1] if x > 0 else 0) + \
+                row_sum + value_square
+
+            row_sum += value_square
+
+    return integral_image_of_squares
+
+
+def sum_region(integral_img_arr, top_left, bottom_right):
     if top_left == bottom_right:
         return integral_img_arr[top_left]
 

Utils.py
@@ -1,6 +1,5 @@
 import numpy as np
 from PIL import Image
-from functools import partial
 from Area import Area
 import os
 
@@ -9,16 +8,22 @@ def load_images(path):
     images = []
     for _file in os.listdir(path):
         if _file.endswith('.jpg'):
-            img_arr = np.array(Image.open((os.path.join(path, _file))), dtype=np.uint64)
-            images.append(img_arr)
+            img_arr = np.asarray(Image.open((os.path.join(path, _file))), dtype=np.uint64)
+
+            # Convert RGB to greyscale
+            images.append(
+                np.transpose(
+                    np.floor(np.dot(img_arr[..., :3], [0.3, 0.59, 0.11]))
+                )
+            )
     return images
 
 
 def is_similar(first_area, second_area):
     delta = first_area.size * 0.2
 
-    delta_x = abs(first_area.x - second_area.y)
-    delta_y = abs(first_area.x - second_area.y)
+    delta_x = abs(first_area.x - second_area.x)
+    delta_y = abs(first_area.y - second_area.y)
     delta_size = abs(first_area.size - second_area.size)
 
     if delta_x <= delta and delta_y <= delta and delta_size <= delta:
@@ -38,9 +43,9 @@ def combine_neighbours(neighbours):
         )
 
     result.set(
-        result.x / len(neighbours),
-        result.y / len(neighbours),
-        result.size / len(neighbours)
+        int(result.x / len(neighbours)),
+        int(result.y / len(neighbours)),
+        int(result.size / len(neighbours))
     )
 
     return result
@@ -64,3 +69,4 @@ def merge(areas, min_neighbors):
             result.append(combine_neighbours(neighbours))
 
     return result
+